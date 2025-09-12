30328c136b761172e384c716bb799b2528119c69
ninoell55 <ninoadityooo@gmail.com>
2025-09-12 10:23:28 +0700

    Membuat koneksi ke database

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,13 @@
 <!-- Halaman Koneksi Database -->
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "belajar_github";
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi Gagal: " . $conn->connect_error);
+}
+// echo "Koneksi Berhasil";
+?>
