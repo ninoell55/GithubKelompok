e369d6b807a799fe7d6f2e933db18746d1ef0f20
jiaa220809 <jihansyahira228@gmail.com>
2025-09-15 09:50:04 +0700

    ini hapus baru

--- pages/hapus.php
+++ pages/hapus.php
@@ -3,7 +3,7 @@
 require_once '../koneksi.php';
 
 $id = $_GET['id'];
-$query = "DELETE FROM siswa WHERE id = '$id'";
+$query = "DELETE FROM siswa WHERE id_siswa = '$id'";
 
 $result = mysqli_query($conn, $query);
 
